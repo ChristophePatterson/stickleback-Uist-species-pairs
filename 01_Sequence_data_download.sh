b28f56946f559cf0b835e48c6a0ec1d73f630986
Christophe Patterson <christophepatterson@outlook.com>
2025-03-20 15:44:08 +0000

    Change total number of files intially downloaded

--- 01_Sequence_data_download.sh
+++ 01_Sequence_data_download.sh
@@ -23,10 +23,10 @@ module load rclone-uon/1.65.2
 # Get complete list of all files in back up folder
 rclone lsf MacColl_stickleback_lab_2:HPC_data_backup/bigdata/trimmed_fqs > seq_list.txt
 ## Just get sequence files with Uist in name
-grep Uist seq_list.txt | head -n 20 > seq_list_uist.txt
+grep Uist seq_list.txt | head -n 30 > seq_list_uist.txt
 
 # Copy over wanted list of sequence files
-rclone copy --files-from seq_list_uist.txt MacColl_stickleback_lab_2:HPC_data_backup/bigdata/trimmed_fqs ~/data/sticklebacks/
+rclone copy --files-from seq_list_uist.txt MacColl_stickleback_lab_2:HPC_data_backup/bigdata/trimmed_fqs ~/data/sticklebacks/seq/
 
 # unload the rclone module
 module unload rclone-uon/1.65.2
