9626bb968826526794246cc1b20bd8b32dacc93e
Christophe Patterson <christophepatterson@outlook.com>
2025-03-31 10:37:06 +0100

    Update so seq download to include multiple sharepoints sites

--- 01_Sequence_data_download.sh
+++ 01_Sequence_data_download.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=40g
-#SBATCH --time=48:00:00
+#SBATCH --time=72:00:00
 #SBATCH --job-name=sequence_data_download
 #SBATCH --output=/gpfs01/home/mbzcp2/slurm_outputs/slurm-%x-%j.out
 
@@ -24,20 +24,20 @@ module load rclone-uon/1.65.2
 output_dir=(~/data/sticklebacks/seq/)
 mkdir -p $output_dir
 
-# Get complete list of all files in back up folder
-rclone lsf MacColl_stickleback_lab_2:HPC_data_backup/bigdata/trimmed_fqs > seq_list.txt
-## Just get sequence files with Uist in name
-grep Uist seq_list.txt > seq_list_uist.txt
-awk -F "\"*,\"*" '$8==2' /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/bigdata_metadata_ADCM_MB.csv | \awk -F "\"*,\"*" {'print $2'} > sample_names.txt
-## Get iceland outgroup
-awk -F "\"*,\"*" '$4=="Iceland" && $10=="resi"' /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/bigdata_metadata_ADCM_MB.csv | \awk -F "\"*,\"*" {'print $2'} >> sample_names.txt
-# Get sequence names
-awk '{print $1 "_R1.fastq.gz"}' sample_names.txt > sample_pairs_seq.txt
-awk '{print $1 "_R2.fastq.gz"}' sample_names.txt >> sample_pairs_seq.txt
+## Create list of files from 
+bigdata=(/gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/bigdata_Christophe_2025-03-28.csv)
+awk -F "," '{print $5 "/" $2}' $bigdata > seq_list.txt
+awk -F "," '{print $5 "/" $3}' $bigdata >> seq_list.txt
 
+# Remove excess file path and split into the two sharepoint back up location (has to be done separately)
+grep "sites/MacColl_stickleback_lab_2/Shared Documents/" seq_list.txt |  sed 's/^.\{49\}//' > MacColl_stickleback_lab_2_seq_files.txt
+grep "sites/MacCollSticklebackLab/Shared Documents/" seq_list.txt |  sed 's/^.\{45\}//' > MacCollSticklebackLab_seq_files.txt
 
 # Copy over wanted list of sequence files
-rclone --bwlimit 100M --checkers 4 --transfers 4 --onedrive-chunk-size 5M -q copy --files-from sample_pairs_seq.txt MacColl_stickleback_lab_2:HPC_data_backup/bigdata/trimmed_fqs $output_dir
+# From MacCollSticklebackLab
+rclone --bwlimit 100M --checkers 4 --transfers 4 --onedrive-chunk-size 5M -q copy --files-from MacCollSticklebackLab_seq_files.txt MacCollSticklebackLab: $output_dir
+# From MacColl_stickleback_lab_2
+rclone --bwlimit 100M --checkers 4 --transfers 4 --onedrive-chunk-size 5M -q copy --files-from MacColl_stickleback_lab_2_seq_files.txt MacColl_stickleback_lab_2: $output_dir
 
 # unload the rclone module
 module unload rclone-uon/1.65.2
