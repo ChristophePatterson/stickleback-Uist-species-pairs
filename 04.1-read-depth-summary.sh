86091e0ccbced232175ea81328dba98e4aedd9f1
Christophe Patterson <christophepatterson@outlook.com>
2025-04-07 12:56:35 +0100

    Expansion of bam stat anaylsis in R

--- 04.1-read-depth-summary.sh
+++ 04.1-read-depth-summary.sh
@@ -40,6 +40,8 @@ do
     ## Get bam stats file
     QC=$(echo ${filepath}genome_results.txt)
     ## Extract varibles  of interest (any new added make sure to add colname at top)
+    # location of bamfile
+    bam_file=$(sed -n 's/     (bam file = //p' $QC)
     # number of reads
     reads=$(sed -n 's/     number of reads = //p' $QC)
     # Number of mapped reads
@@ -53,11 +55,10 @@ do
     # Number of duplicated reads
     dup_reads=$(sed -n 's/     number of duplicated reads (estimated) = //p'  $QC)
     ## Print out to one file
-    echo -e "${filename}\t${reads}\t${map_reads}\t${mn_cov}\t${std_cov}\t${map_qlty}\t${dup_reads}" >> $out_filepath/global_raw_report_custom.txt
+    echo -e "${filename}\t$bam_file\t${reads}\t${map_reads}\t${mn_cov}\t${std_cov}\t${map_qlty}\t${dup_reads}" >> $out_filepath/global_raw_report_custom.txt
 done
 
 Rscript /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/04.2-read-depth-summary-plots.R $out_filepath/global_raw_report_custom.txt $out_filepath
-
 
 ## Run Qualimap
 ~/apps/qualimap_v2.3/qualimap multi-bamqc -d qualimap.tmp.txt \
--- 04.2-read-depth-summary-plots.R
+++ 04.2-read-depth-summary-plots.R
@@ -1,6 +1,8 @@
 ## Load packages
+.libPaths(c("/gpfs01/home/mbzcp2/R/x86_64-pc-linux-gnu-library/4.2", .libPaths()))
 library(ggplot2)
 library(patchwork)
+library(ggrepel)
 
 ## Get command line argument
 args <- commandArgs(trailingOnly = TRUE)
@@ -18,13 +20,36 @@ bam_QC <- merge(bam_QC, sample_data[,c(1,6:15)], by.x = "sample", by.y = "indivi
 
 ## Convert to numerics
 bam_QC$mn_coverage <- as.numeric(gsub("X", "", x =  bam_QC$mn_coverage))
+bam_QC$std_coverage <- as.numeric(gsub("X", "", x =  bam_QC$std_coverage))
 bam_QC$reads <- as.numeric(gsub(",", "", x =  bam_QC$reads))
+bam_QC$mapped_reads <- as.numeric(gsub(",", "", x =  bam_QC$mapped_reads))
+bam_QC$dupl_reads <- as.numeric(gsub(",", "", x =  bam_QC$dupl_read))
 
 ## Plot
 p <- ggplot(bam_QC) +
-  geom_point(aes(mn_coverage, reads, col = Ecotype))
+  geom_point(aes(mn_coverage, mapped_reads, col = Ecotype))
 q <- ggplot(bam_QC) +
-  geom_boxplot(aes(Ecotype, mn_coverage, col = Ecotype)) +
-  geom_jitter(aes(Ecotype, mn_coverage, col = Ecotype), height = 0)
+  geom_boxplot(aes(Ecotype, mn_coverage, col = Ecotype), outlier.shape = NA) +
+  geom_jitter(aes(Ecotype, mn_coverage, col = Ecotype), height = 0) +
+  geom_hline(yintercept = 5, col = "red")
+s <- ggplot(bam_QC) +
+  geom_boxplot(aes(Ecotype, Ave_map_qc, col = Ecotype), outlier.shape = NA) +
+  geom_jitter(aes(Ecotype, Ave_map_qc, col = Ecotype), height = 0) +
+  geom_hline(yintercept = 10, col = "red")
+r <- ggplot(bam_QC) +
+  geom_boxplot(aes(Population, mn_coverage, col = Ecotype), outlier.shape = NA) +
+  geom_jitter(aes(Population, mn_coverage, col = Ecotype), height = 0) +
+  geom_hline(yintercept = 5, col = "red") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+t <- ggplot(bam_QC) +
+  geom_boxplot(aes(Ecotype, dupl_reads/mapped_reads, col = Ecotype), outlier.shape = NA) +
+  geom_jitter(aes(Ecotype, dupl_reads/mapped_reads, col = Ecotype), height = 0)
+
+p1 <- (p /q / s / r / t)
 # Save
-ggsave(filename = "Mapping_coverage.png", p + q, width = 10, height = 5)
+ggsave(filename = "Mapping_coverage.png", p1, width = 10, height = 20)
+
+###  Filter out files with low coverage
+min_cov <- 5
+min_QC <- 10
+bam_QC_hiQ <- bam_QC[bam_QC$mn_coverage>=min_cov&bam_QC$Ave_map_qc>=min_QC,]
