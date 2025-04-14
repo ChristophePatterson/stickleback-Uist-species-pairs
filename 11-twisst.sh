f6efe1986344e8d9605944f27920956fb6597a03
Christophe Patterson <christophepatterson@outlook.com>
2025-04-14 14:36:09 +0100

    Include popfile and individual file for config of twisst

--- 11-twisst.sh
+++ 11-twisst.sh
@@ -20,6 +20,9 @@
 module load PhyML/3.3.20220408-foss-2023a
 module load R-uoneasy/4.2.1-foss-2022a
 
+# Open conda enviroment
+conda activate twisst-stickleback
+
 # set variables
 wkdir=/gpfs01/home/mbzcp2/data/sticklebacks
 species=stickleback
@@ -35,13 +38,26 @@ else
    scancel "$SLURM_JOB_ID"
 fi
 
-## Create list of individuals to use (# include | shuf | head -n 10 to reduce sample input)
-grep -f $wkdir/vcfs/${species}_subset_samples.txt /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/species_pairs_sequence_data.csv | 
-    awk -F ',' '{ print $1}' | shuf | head -n 10 > $wkdir/results/twisst/ind_file.txt
+## Create list of individuals to use (# include | shuf | head -n 10 to reduce sample input and not include iceland for the moment)
+grep -f $wkdir/vcfs/${species}_subset_samples_withOG.txt /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/species_pairs_sequence_data.csv | \
+   grep -v -E 'Iceland' | shuf | head -n 20 |  awk -F ',' -v OFS='\t' '{ print $1, $13}' > $wkdir/results/twisst/pop_file.txt
+# Include Iceland
+grep -f $wkdir/vcfs/${species}_subset_samples_withOG.txt /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/species_pairs_sequence_data.csv | \
+   grep -E 'Iceland' | awk -F ',' -v OFS='\t' '{ print $1, $13}' | sed 's/NA/Ice/' >> $wkdir/results/twisst/pop_file.txt
+
+## Create individual file with just sample name
+awk '{ print $1 }' $wkdir/results/twisst/pop_file.txt > $wkdir/results/twisst/ind_file.txt
+
+cat $wkdir/results/twisst/pop_file.txt
 
 ### Run Genomics general script for calculating trees over a sliding window
 ## Best to use one thread because it doesnt take that line
 ## NOTE THIS CODE HAS BEEN MOVED INTO THE MAIN GENOMICS GENERAL DIRECTORY SO IT CAN ACCESS THE GENOMICS.PY SCRIPT
-python ~/apps/genomics_general/phyml_sliding_windows.py -T $SLURM_CPUS_PER_TASK -g $wkdir/vcfs/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2_SpPair.geno.gz \
+python ~/apps/genomics_general/phyml_sliding_windows.py -T 1 -g $wkdir/vcfs/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2_SpPair-wOG.geno.gz \
    --prefix $wkdir/results/twisst/${species}.phyml_bionj.w50 --windType sites --model GTR --windSize 1000 -O 0 -M 1 -Ms 1 --indFile $wkdir/results/twisst/ind_file.txt
 
+### Running Twisst (require install of ete3)
+python ~/apps/twisst/twisst.py --help
+
+python ~/apps/twisst/twisst.py -t $wkdir/results/twisst/${species}.phyml_bionj.w50.trees.gz -w $wkdir/results/twisst/${species}.weights.csv.gz \
+   -g anad -g resi -g fw -g Ice --method complete --groupsFile $wkdir/results/twisst/pop_file.txt
