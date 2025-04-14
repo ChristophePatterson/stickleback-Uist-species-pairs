44bce737f4505167af2bf9bd80409b07c3ea4210
Christophe Patterson <christophepatterson@outlook.com>
2025-04-14 15:35:08 +0100

    In twisst code allows for varible window selection and inclusion of plotting output in R

--- 11-twisst.sh
+++ 11-twisst.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=3
 #SBATCH --mem=20g
 #SBATCH --time=18:00:00
 #SBATCH --job-name=sliding-window
@@ -19,6 +19,7 @@
 # load modules
 module load PhyML/3.3.20220408-foss-2023a
 module load R-uoneasy/4.2.1-foss-2022a
+conda activate twisst-stickleback
 
 # Open conda enviroment
 conda activate twisst-stickleback
@@ -29,6 +30,10 @@ species=stickleback
 
 ## Create output directory
 mkdir -p $wkdir/results/twisst
+########################
+## Choose window width
+########################
+mywindow=(100)
 
 ## Test is Geno file has been created (may need to run whole of 10-sliding window code)
 if [ -f $wkdir/vcfs/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2_SpPair.geno.gz ]; then
@@ -40,28 +45,28 @@ fi
 
 ## Create list of individuals to use (# include | shuf | head -n 10 to reduce sample input and not include iceland for the moment)
 grep -f $wkdir/vcfs/${species}_subset_samples_withOG.txt /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/species_pairs_sequence_data.csv | \
-   grep -v -E 'Iceland' | shuf | head -n 20 |  awk -F ',' -v OFS='\t' '{ print $1, $13}' > $wkdir/results/twisst/pop_file.txt
+   grep -v -E 'Iceland' | shuf | head -n 20 |  awk -F ',' -v OFS='\t' '{ print $1, $13}' > $wkdir/results/twisst/pop_file_w$mywindow.txt
 # Include Iceland
 grep -f $wkdir/vcfs/${species}_subset_samples_withOG.txt /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/species_pairs_sequence_data.csv | \
-   grep -E 'Iceland' | awk -F ',' -v OFS='\t' '{ print $1, $13}' | sed 's/NA/Ice/' >> $wkdir/results/twisst/pop_file.txt
+   grep -E 'Iceland' | awk -F ',' -v OFS='\t' '{ print $1, $13}' | sed 's/NA/Ice/' >> $wkdir/results/twisst/pop_file_w$mywindow.txt
 
 ## Add in new phased sample names 
-awk -v OFS='\t' '{ print $1"_A", $2}' $wkdir/results/twisst/pop_file.txt > $wkdir/results/twisst/phased_pop_file.txt
-awk -v OFS='\t' '{ print $1"_B", $2}' $wkdir/results/twisst/pop_file.txt >> $wkdir/results/twisst/phased_pop_file.txt
+awk -v OFS='\t' '{ print $1"_A", $2}' $wkdir/results/twisst/pop_file_w$mywindow.txt > $wkdir/results/twisst/phased_pop_file_w$mywindow.txt
+awk -v OFS='\t' '{ print $1"_B", $2}' $wkdir/results/twisst/pop_file_w$mywindow.txt >> $wkdir/results/twisst/phased_pop_file_w$mywindow.txt
 
 ## Create individual file with just sample name
 awk '{ print $1 }' $wkdir/results/twisst/pop_file.txt > $wkdir/results/twisst/ind_file.txt
 
-cat $wkdir/results/twisst/pop_file.txt
+cat $wkdir/results/twisst/pop_file_w$mywindow.txt
 
 ### Run Genomics general script for calculating trees over a sliding window
 ## Best to use one thread because it doesnt take that line
 ## NOTE THIS CODE HAS BEEN MOVED INTO THE MAIN GENOMICS GENERAL DIRECTORY SO IT CAN ACCESS THE GENOMICS.PY SCRIPT
 python ~/apps/genomics_general/phyml_sliding_windows.py -T 1 -g $wkdir/vcfs/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2_SpPair-wOG.geno.gz \
-   --prefix $wkdir/results/twisst/${species}.phyml_bionj.w50 --windType sites --model GTR --windSize 1000 -O 0 -M 1 -Ms 1 --indFile $wkdir/results/twisst/ind_file.txt
+   --prefix $wkdir/results/twisst/${species}.phyml_bionj.w$mywindow --windType sites --model GTR --windSize $mywindow -O 0 -M 1 -Ms 1 --indFile $wkdir/results/twisst/ind_file_w$mywindow.txt
 
-### Running Twisst (require install of ete3)
-python ~/apps/twisst/twisst.py --help
+Running Twisst (require install of ete3)
+python ~/apps/twisst/twisst.py -t $wkdir/results/twisst/${species}.phyml_bionj.w$mywindow.trees.gz -w $wkdir/results/twisst/${species}.phyml_bionj.w$mywindow.weights.tsz.gz \
+   -g anad -g resi -g fw -g Ice --method complete --groupsFile $wkdir/results/twisst/phased_pop_file_w$mywindow.txt
 
-python ~/apps/twisst/twisst.py -t $wkdir/results/twisst/${species}.phyml_bionj.w50.trees.gz -w $wkdir/results/twisst/${species}.weights.csv.gz -o $wkdir/results/twisst/${species}.topologies.trees\
-   -g anad -g resi -g fw -g Ice --method complete --groupsFile $wkdir/results/twisst/phased_pop_file.txt
+Rscript /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/11.1-twisst.R $wkdir/results/twisst/${species}.phyml_bionj.w$mywindow
