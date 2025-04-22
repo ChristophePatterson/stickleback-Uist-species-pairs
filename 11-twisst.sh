7c4646da4827a780bfee50a0161484d6e06b8759
Christophe Patterson <christophepatterson@outlook.com>
2025-04-22 14:36:01 +0100

    Fixed twisst analysis for population

--- 11-twisst.sh
+++ 11-twisst.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=20g
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --job-name=sliding-window
 #SBATCH --output=/gpfs01/home/mbzcp2/slurm_outputs/twisst-%x-%j.out
   
@@ -37,10 +37,10 @@ mkdir -p $output_dir
 ########################
 ## Choose window width
 ########################
-mywindow=(100)
+mywindow=(1000)
 # Alter is population level should be used for Ecotype or for individual populations "Ecotype" OR "population"
 # pop_level=("Population")
-pop_level=("complete")
+pop_level=("Population")
 
 # Set weighting method "complete" or "fixed"
 weight_method=("fixed")
@@ -65,12 +65,12 @@ else
 if [[ $pop_level == "Ecotype" ]]; then
    grep -f $wkdir/vcfs/${species}_subset_samples_withOG.txt /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/species_pairs_sequence_data.csv | \
       grep -v -E 'Iceland' | awk -F ',' -v OFS='\t' '{ print $1, $13}' > $output_dir/pop_file_${run_name}.txt
+   # Include Iceland
+   grep -f $wkdir/vcfs/${species}_subset_samples_withOG.txt /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/species_pairs_sequence_data.csv | \
+      grep -E 'Iceland' | awk -F ',' -v OFS='\t' '{ print $1, $13}' | sed 's/NA/Ice/' >> $output_dir/pop_file_${run_name}.txt
    fi
 fi
 
-# Include Iceland
-grep -f $wkdir/vcfs/${species}_subset_samples_withOG.txt /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/species_pairs_sequence_data.csv | \
-   grep -E 'Iceland' | awk -F ',' -v OFS='\t' '{ print $1, $13}' | sed 's/NA/Ice/' >> $output_dir/pop_file_${run_name}.txt
 
 awk '{ print $1 }' $output_dir/pop_file_${run_name}.txt > $output_dir/ind_file_${run_name}.txt
 
@@ -81,17 +81,31 @@ awk -v OFS='\t' '{ print $1"_B", $2}' $output_dir/pop_file_${run_name}.txt >> $o
 ## Create individual file with just sample name
 awk '{ print $1 }' $output_dir/phased_pop_file_${run_name}.txt > $output_dir/phased_ind_file_${run_name}.txt
 
-cat $output_dir/pop_file_${run_name}.txt
-
 ### Run Genomics general script for calculating trees over a sliding window
 ## Best to use one thread because it doesnt take that line
 ## NOTE THIS CODE HAS BEEN MOVED INTO THE MAIN GENOMICS GENERAL DIRECTORY SO IT CAN ACCESS THE GENOMICS.PY SCRIPT
 python ~/apps/genomics_general/phyml_sliding_windows.py -T 1 -g $wkdir/vcfs/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2_SpPair-wOG.geno.gz \
   --prefix $output_dir/${run_name} --windType sites --model GTR --windSize $mywindow -O 0 -M 1 -Ms 1 --indFile $output_dir/ind_file_${run_name}.txt
 
+# CHECK
+echo "PHYML RUN SUCCESFULLY"
+
+echo "Print out all populations in input files"
+awk '{ print $2 }' $output_dir/phased_pop_file_${run_name}.txt | sort | uniq
+
 ## Running Twisst (require install of ete3)
-python ~/apps/twisst/twisst.py -t $output_dir/${run_name}.trees.gz -w $output_dir/${run_name}.weights.tsv.gz \
-   -g anad -g resi -g fw -g Ice --method ${weight_method} --groupsFile $output_dir/phased_pop_file_${run_name}.txt
+## Changes -g parameter based on whether using Ecotype or populations
+if [[ $pop_level == "Population" ]]; then
+   python ~/apps/twisst/twisst.py -t $output_dir/${run_name}.trees.gz -w $output_dir/${run_name}.weights.tsv.gz \
+       -g CLAC -g CLAM -g DUIM -g DUIN -g LUIB -g LUIM -g OBSE -g OBSM \
+       --method ${weight_method} --groupsFile $output_dir/phased_pop_file_${run_name}.txt
+else
+if [[ $pop_level == "Ecotype" ]]; then
+   python ~/apps/twisst/twisst.py -t $output_dir/${run_name}.trees.gz -w $output_dir/${run_name}.weights.tsv.gz \
+   -  g anad -g resi -g fw -g Ice --method ${weight_method} --groupsFile $output_dir/phased_pop_file_${run_name}.txt
+   fi
+fi
+
 
 module purge
 ## Load R module (DO NOT MOVE TO START OF SCRIPT AS IT BREAKS THE PYTHON VERSION)
