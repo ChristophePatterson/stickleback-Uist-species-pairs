6b7571e213e17134202177b0cb8e98d058231163
Christophe Patterson <christophepatterson@outlook.com>
2025-05-16 15:41:35 +0100

    TT plot R able to tackle multiple chromosomes

--- 2_Results/2_2_Divergence_estimates/13.5-TT-plot.R
+++ 2_Results/2_2_Divergence_estimates/13.5-TT-plot.R
@@ -1,11 +1,14 @@
 # Rscriot tt.R <path to TT calculation> <output_folder>
-## Taken from https://github.com/popgenDK/seqAfrica_giraffe
+## Code to produce custom plots from https://github.com/popgenDK/seqAfrica_giraffe
 library(tidyverse)
+library(patchwork)
 
 args <- commandArgs(trailingOnly=T)
 
 inTT <- args[1]
 outprefix <- args[2]
+
+pop_pair <- basename(outprefix)
 
 TTcalcs.files <- list.files(inTT)
 
@@ -17,7 +20,10 @@ TTcalcs <- cbind(TTcalcs.files, TTcalcs)
 
 colnames(TTcalcs) <- c("TTparms","T1", "T2", "Na")
 
-TTcalcs$range <- gsub(".tt.split.years", "",gsub("OBSE_OBSM_chr1_", "", TTcalcs$TTparms))
+head(TTcalcs$TTparms)
+
+TTcalcs$range <- gsub(".tt.split.years", "", gsub(TTcalcs$TTparms, pattern = paste0(paste0(pop_pair, "_chr",1:22,"_"), collapse = '|'), replacement  = ""))
+TTcalcs$chr <- stringr::str_split_i(TTcalcs$TTparms,"_", 3)
 
 TTcalcs$start <- as.numeric(stringr::str_split_i(TTcalcs$range,"-", 1))
 TTcalcs$end <- as.numeric(stringr::str_split_i(TTcalcs$range,"-", 2))
@@ -27,18 +33,29 @@ TTcalcs$T2 <- as.numeric(TTcalcs$T2)
 ## Reorder calcules
 TTcalcs <- TTcalcs[order(TTcalcs$start),]
 
-p <- ggplot(TTcalcs) +
-    geom_histogram(aes(x = T1), fill = "blue", alpha = 0.5) +
-    geom_histogram(aes(x = T2), fill = "red", alpha = 0.5) +
-    ggtitle(outprefix)
+summary(TTcalcs$T1)
+summary(TTcalcs$T2)
 
-ggsave(paste0(outprefix, "_T1andT2_hist.pdf"), p, width = 10, height = 10)
+p <- ggplot(TTcalcs) +
+    geom_histogram(aes(x = T1), fill = "blue", alpha = 0.5, bins = 100) +
+    geom_histogram(aes(x = T2), fill = "red", alpha = 0.5, bins = 100) +
+    ggtitle(pop_pair)
+
+q <- ggplot(TTcalcs) +
+    geom_boxplot(aes(y = T1, x = "T1"), outlier.shape = NA) +
+    geom_boxplot(aes(y = T2, x = "T2"), outlier.shape = NA) +
+    ggtitle(pop_pair)   
+
+ggsave(paste0("test.pdf"), p+q, width = 10, height = 10)
+ggsave(paste0(outprefix, "_T1andT2_hist.pdf"), p+q, width = 10, height = 10)
 
 p <- ggplot(TTcalcs) +
     geom_point(aes(x = start, y = T1/1000), col = "red") +
     geom_point(aes(x = start, y = T2/1000), col = "blue") +
     ylab("Year (ka)") +
-    ggtitle(outprefix)
+    facet_grid(.~chr, scale = "free_x", space = "free_x") +
+    ggtitle(pop_pair)
+
 
 ggsave(paste0(outprefix, "_T1andT2.pdf"), p, width = 10, height = 5)
 
