6351c6ca6da1f13d5acd2993045698c11ff845b6
Christophe Patterson <christophepatterson@outlook.com>
2025-05-16 12:15:12 +0100

    TT loops through all chromosomes

--- 2_Results/2_2_Divergence_estimates/13.3-TTcalculations.sh
+++ 2_Results/2_2_Divergence_estimates/13.3-TTcalculations.sh
@@ -9,7 +9,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=20g
 #SBATCH --time=48:00:00
-#SBATCH --array=1-10
+#SBATCH --array=1,18,31,40,45
 #SBATCH --job-name=TTcalculator
 #SBATCH --output=/gpfs01/home/mbzcp2/slurm_outputs/slurm-%x-%j.out
 
@@ -24,6 +24,9 @@ species=stickleback
 ## Pop Combination file
 PopComb=($wkdir/results/TTmethod/vcfs/top_coverage_samples_PopComb.txt)
 TopSamps=($wkdir/results/TTmethod/vcfs/top_coverage_samples.txt)
+# Reference chromosome
+genome_info=(/gpfs01/home/mbzcp2/data/sticklebacks/genomes/GCF_016920845.1_sequence_report.tsv)
+
 
 ## Make array equal to the number of population pairs to make comparison
 Pop1=$(awk -F ',' "FNR==$SLURM_ARRAY_TASK_ID" $PopComb | awk '{ print $1 }')
@@ -41,70 +44,82 @@ result_out=($wkdir/results/TTmethod/TTresults/${Pop1}_${Pop2})
 mkdir -p $vcf_out
 mkdir -p $result_out
 
-# If vcf hasn't been indiexed index for sample 1
-if [ ! -f $wkdir/results/TTmethod/vcfs/${sample1}/${sample1}_chr1.vcf.gz.tbi ]; then
-    	tabix $wkdir/results/TTmethod/vcfs/${sample1}/${sample1}_chr1.vcf.gz
-fi
-# and for sample 2
-if [ ! -f $wkdir/results/TTmethod/vcfs/${sample2}/${sample2}_chr1.vcf.gz.tbi ]; then
-    	tabix $wkdir/results/TTmethod/vcfs/${sample2}/${sample2}_chr1.vcf.gz
-fi
 
-## Create merged bcf for each of the two samples
-bcftools merge \
-    -o $vcf_out/${Pop1}_${Pop2}_chr1.vcf.gz \
-    -O v \
-    --threads $SLURM_CPUS_PER_TASK \
-    /gpfs01/home/mbzcp2/data/sticklebacks/results/TTmethod/vcfs/${sample1}/${sample1}_chr1.vcf.gz \
-    /gpfs01/home/mbzcp2/data/sticklebacks/results/TTmethod/vcfs/${sample2}/${sample2}_chr1.vcf.gz
-
-tabix $vcf_out/${Pop1}_${Pop2}_chr1.vcf.gz
-
-## Remove indels and sites that have more than 2 allele varients and sites with missing base calles
-bcftools view --max-alleles 2 --exclude-types indels -e 'GT=="./."' -O v -o $vcf_out/${Pop1}_${Pop2}_filt_chr1.vcf.gz $vcf_out/${Pop1}_${Pop2}_chr1.vcf.gz
-tabix $vcf_out/${Pop1}_${Pop2}_filt_chr1.vcf.gz
-# remove old vcf and tbi
-rm $vcf_out/${Pop1}_${Pop2}_chr1.vcf.gz
-rm $vcf_out/${Pop1}_${Pop2}_chr1.vcf.gz.tbi
-
-# Create output directories
-mkdir -p $result_out/sfs
-mkdir -p $result_out/TTcals
-mkdir -p $result_out/TTresults
-
-# Start position for chromosome
-start_pos=(1)
-# Number of bases to shift up by 
-inc_pos=(50000)
-# Starting end position (start position + increment)
-end_pos=$((start_pos+inc_pos))
-# Hight called bases in vcf to stop at
-stop_pos=$(bcftools query  -f '%POS\n' $vcf_out/${Pop1}_${Pop2}_filt_chr1.vcf.gz | tail -n 1)
-
-## Starting at the first section calculate TT for all sections of the chromosome
-while [ "$end_pos" -le "$stop_pos" -o "$start_pos" -ge "$stop_pos" ]
+### Loop throug indivudal chromosomes (exculusing X (19) and Y (22))
+for chr in {1..18} {20..21}
 do
-    ## Remove 
-    bcftools view -r NC_053212.1:$start_pos-$end_pos $vcf_out/${Pop1}_${Pop2}_filt_chr1.vcf.gz | \
-    bcftools query -f "[%GT ]\n" | awk '{{a[$1"_"$2]++}} END{{for(g in a){{print g, a[g]}}}}' > $result_out/sfs/${Pop1}_${Pop2}_chr1_${start_pos}-${end_pos}.sfs
+    # Extract scaffold name from genome info file (need to add on to number fo avoid header)
+    scaf=$(awk -F ',' -v line=$((chr + 1)) "FNR==line" $genome_info | awk '{ print $9 }')
+    echo "Starting Chromosome $chr using scafffold $scaf"
+    # Create text chrI for easier to need file extraction code
+    chrI=$(echo chr$chr)
 
-    # Rscript TT-calculator.R <path to unfolded 2dsfs> <prefix for output files> <assumed mutation rate> <assumed generation time>
-    Rscript /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/2_Results/2_2_Divergence_estimates/13.4-TT-calculator.R \
-    $result_out/sfs/${Pop1}_${Pop2}_chr1_${start_pos}-${end_pos}.sfs \
-    $result_out/TTcals/${Pop1}_${Pop2}_chr1_${start_pos}-${end_pos} \
-    "2.1e-08" \
-    "1"
-    ## Increase section of chromsome being used
-    end_pos=$((end_pos+inc_pos))
-    start_pos=$((start_pos+inc_pos))
-    echo $start_pos
+    # If vcf hasn't been indiexed index for sample 1
+    if [ ! -f $wkdir/results/TTmethod/vcfs/${sample1}/${sample1}_${chrI}.vcf.gz.tbi ]; then
+        	tabix $wkdir/results/TTmethod/vcfs/${sample1}/${sample1}_${chrI}.vcf.gz
+    fi
+    # and for sample 2
+    if [ ! -f $wkdir/results/TTmethod/vcfs/${sample2}/${sample2}_${chrI}.vcf.gz.tbi ]; then
+        	tabix $wkdir/results/TTmethod/vcfs/${sample2}/${sample2}_${chrI}.vcf.gz
+    fi
 
+    ## Create merged bcf for each of the two samples
+    bcftools merge \
+        -o $vcf_out/${Pop1}_${Pop2}_${chrI}.vcf.gz \
+        -O v \
+        --threads $SLURM_CPUS_PER_TASK \
+        /gpfs01/home/mbzcp2/data/sticklebacks/results/TTmethod/vcfs/${sample1}/${sample1}_${chrI}.vcf.gz \
+        /gpfs01/home/mbzcp2/data/sticklebacks/results/TTmethod/vcfs/${sample2}/${sample2}_${chrI}.vcf.gz
+
+    tabix $vcf_out/${Pop1}_${Pop2}_${chrI}.vcf.gz
+
+    ## Remove indels and sites that have more than 2 allele varients and sites with missing base calles
+    bcftools view --max-alleles 2 --exclude-types indels -e 'GT=="./."' -O v -o $vcf_out/${Pop1}_${Pop2}_filt_${chrI}.vcf.gz $vcf_out/${Pop1}_${Pop2}_${chrI}.vcf.gz
+    tabix $vcf_out/${Pop1}_${Pop2}_filt_${chrI}.vcf.gz
+    # remove old vcf and tbi
+    rm $vcf_out/${Pop1}_${Pop2}_${chrI}.vcf.gz
+    rm $vcf_out/${Pop1}_${Pop2}_${chrI}.vcf.gz.tbi
+
+    # Create output directories
+    mkdir -p $result_out/sfs
+    mkdir -p $result_out/TTcals
+    mkdir -p $result_out/TTresults
+
+    # Start position for chromosome
+    start_pos=(1)
+    # Number of bases to shift up by 
+    inc_pos=(50000)
+    # Starting end position (start position + increment)
+    end_pos=$((start_pos+inc_pos))
+    # Hight called bases in vcf to stop at
+    stop_pos=$(bcftools query  -f '%POS\n' $vcf_out/${Pop1}_${Pop2}_filt_${chrI}.vcf.gz | tail -n 1)
+
+    ## Starting at the first section calculate TT for all sections of the chromosome
+    while [ "$end_pos" -le "$stop_pos" -o "$start_pos" -ge "$stop_pos" ]
+    do
+        ## Remove 
+        bcftools view -r ${scaf}:$start_pos-$end_pos $vcf_out/${Pop1}_${Pop2}_filt_${chrI}.vcf.gz | \
+        bcftools query -f "[%GT ]\n" | awk '{{a[$1"_"$2]++}} END{{for(g in a){{print g, a[g]}}}}' > $result_out/sfs/${Pop1}_${Pop2}_${chrI}_${start_pos}-${end_pos}.sfs
+
+        # Rscript TT-calculator.R <path to unfolded 2dsfs> <prefix for output files> <assumed mutation rate> <assumed generation time>
+        Rscript /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/2_Results/2_2_Divergence_estimates/13.4-TT-calculator.R \
+        $result_out/sfs/${Pop1}_${Pop2}_${chrI}_${start_pos}-${end_pos}.sfs \
+        $result_out/TTcals/${Pop1}_${Pop2}_${chrI}_${start_pos}-${end_pos} \
+        "2.1e-08" \
+        "1"
+        ## Increase section of chromsome being used
+        end_pos=$((end_pos+inc_pos))
+        start_pos=$((start_pos+inc_pos))
+        # echo $start_pos
+
+    done
+## End loop through chromosomes
 done
 
 mkdir -p $wkdir/results/TTmethod/vcfs/test/TTresults
 
 Rscript /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/2_Results/2_2_Divergence_estimates/13.5-TT-plot.R \
         $result_out/TTcals/ \
-        $result_out/TTresults/${Pop1}_${Pop2}_chr1
+        $result_out/TTresults/${Pop1}_${Pop2}
 
 
