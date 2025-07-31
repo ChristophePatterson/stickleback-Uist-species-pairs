d0c5d84e02a247eb6563643645cf47913bfff724
Christophe Patterson <christophepatterson@outlook.com>
2025-07-31 16:29:07 +0100

    Bug fixes to combined twisst plot

--- 2_Results/2_1_Population_genomics/11.3-Twisst-Populations-combined-plot.R
+++ 2_Results/2_1_Population_genomics/11.3-Twisst-Populations-combined-plot.R
@@ -3,14 +3,14 @@ source("~/apps/twisst/plot_twisst.R")
 args <- commandArgs(trailingOnly = TRUE)
 # set path
 top_dir <- args[1]
+# top_dir <- "/gpfs01/home/mbzcp2/data/sticklebacks/results/ploidy_aware_HWEPops_MQ10_BQ20/twisst/Population_comparison"
 
 setwd(top_dir)
-source("~/apps/twisst/plot_twisst.R")
 library(ggplot2)
 
 pop_combn <- read.table("pop_uniq.txt_combn.txt")
 pop_combn$comb <- paste0(pop_combn[,1], "_", pop_combn[,2])
-pop_combn <- pop_combn[-grep("OLAV", pop_combn$comb),]
+# pop_combn <- pop_combn[-grep("OLAV", pop_combn$comb),]
 
 twisst_data_all <- list()
 for(i in 1:length(pop_combn$comb)){
@@ -48,7 +48,7 @@ twisst_data_all <- do.call("rbind.data.frame", twisst_data_all)
 cbPalette <- c("#F0E442","#D55E00","#0072B2","#999999", "#E69F00" , "#56B4E9", "#009E73", "#CC79A7", "black")
 
 ## Swap out seq number for scaffold
-scaf <- read.table("GCF_016920845.1_sequence_report.tsv", sep = "\t", header = T)
+scaf <- read.table("/gpfs01/home/mbzcp2/data/sticklebacks/genomes/GCF_016920845.1_sequence_report.tsv", sep = "\t", header = T)
 twisst_data_all$chr <- scaf$Sequence.name[match(twisst_data_all$scaffold,scaf$RefSeq.seq.accession)]
 
 p <- ggplot(twisst_data_all) +
@@ -57,7 +57,7 @@ p <- ggplot(twisst_data_all) +
   # scale_color_manual(values = topo_cols) +
   theme_classic()
 
+
 ggsave(file = paste0(top_dir, "/All_population_comparison_twisst.pdf"), p, width = 15, height = 30)
 ggsave(file = paste0(top_dir, "/All_population_comparison_twisst.png"), p, width = 15, height = 30)
-
 
