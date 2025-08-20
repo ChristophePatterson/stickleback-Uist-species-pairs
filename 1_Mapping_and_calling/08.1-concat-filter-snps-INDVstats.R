1426958ffc76c709b2bfa232f586746cbe30c796
Christophe Patterson <christophepatterson@outlook.com>
2025-08-20 11:46:27 +0100

    Filter out excess heterozygous samples

--- 1_Mapping_and_calling/08.1-concat-filter-snps-INDVstats.R
+++ 1_Mapping_and_calling/08.1-concat-filter-snps-INDVstats.R
@@ -32,6 +32,15 @@ idf <- tibble(merge(merge(ihet, imiss , by = "INDV"), idepth[,colnames(idepth)!=
 seq_data <- read.csv("/gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/bigdata_Christophe_header_2025-04-28.csv", header = T)
 idf <- tibble(merge(seq_data, idf, by.x = "individual", by.y = "INDV"))
 
+# Calculate prop of heterozygous samples
+idf$prop_het <- 1-(idf$OHOM/idf$N_SITES)
+
+## Calculate excess heterozygousity
+idf <- idf %>%
+  group_by(Population) %>%
+  mutate(excess_het = prop_het-(mean(prop_het)),
+         excess_het_sd = (prop_het-(mean(prop_het))>0.015))
+
 # Plot
 p <- ggplot(idf) +
   geom_boxplot(aes(Population, MEAN_DEPTH), outlier.shape = NA) +
@@ -53,14 +62,23 @@ s <- ggplot(idf[idf$Waterbody%in%paired_waterbodies,]) +
   geom_point(aes(N_MISS, OHOM/N_SITES, col = Population), height = 0, width = 0.2) +
   ggtitle("Sample homozgousity vs missing data")
 
-## Save plot
-plot1 <- p+q+r+s + plot_layout(guides = "collect")
+s1 <- ggplot(idf) +
+  geom_boxplot(aes(Population, prop_het), outlier.shape = NA) +
+  geom_jitter(aes(Population, prop_het, col = excess_het_sd), height = 0, width = 0.2) +
+  ggtitle("Sample homozgousity by population")
 
-ggsave("stats/stats_plot.png", plot1, width = 16, height = 12)
+## Save plot
+#plot1 <- p+q+r+s + plot_layout(guides = "collect")
+
+#ggsave("stats/stats_plot.png", plot1, width = 16, height = 12)
+ggsave("stats/stats_het_pop_plot.png", s1, width = 16, height = 12)
 
 ## Get samples that are high QUAL
-HQsamples <- idf$individual[idf$N_MISS/idf$N_DATA<=0.4]
+HQsamples <- idf$individual[idf$N_MISS/idf$N_DATA<=0.4|idf$excess_het>0.015]
+# Also remove sample NOVSC116 which contains some odd mitochondrial data
+HQsamples <- HQsamples[HQsamples!="NOVSC116"]
 
 print(paste0(sum(idf$N_MISS/idf$N_DATA>=0.4), " samples do not meet critearia for missing data"))
+print(paste0(sum(idf$excess_het>0.015), " samples have excess heterozgousity"))
 writeLines(con = "HiQ_vcf_samples.txt", HQsamples)
 
