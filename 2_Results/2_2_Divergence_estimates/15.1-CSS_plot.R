89b10b8aaa1b63cfdda3ec002601daefc3fb0589
Christophe Patterson <christophepatterson@outlook.com>
2025-08-28 12:12:32 +0100

    CSS stats extraction

--- 2_Results/2_2_Divergence_estimates/15.1-CSS_plot.R
+++ 2_Results/2_2_Divergence_estimates/15.1-CSS_plot.R
@@ -5,9 +5,9 @@ library(tidyverse)
 
 args<-commandArgs(trailingOnly=T)
 CSS.dir <- args[1]
-#CSS.dir <-  "/gpfs01/home/mbzcp2/data/sticklebacks/results/GCA_046562415.1_Duke_GAcu_1.0_genomic/ploidy_aware_HWEPops_MQ10_BQ20/sliding-window/CSS/stickleback.wnd25000.sld5000.mnSNP1.mthbasepair-pca.MAF0.05"
+#CSS.dir <-  "/gpfs01/home/mbzcp2/data/sticklebacks/results/GCA_046562415.1_Duke_GAcu_1.0_genomic/ploidy_aware_HWEPops_MQ10_BQ20/sliding-window/CSS/stickleback.wnd2500.sld500.mnSNP1.mthbasepair-mds.MAF0.05"
 CSS.run <- args[2]
-#CSS.run <- "stickleback.wnd25000.sld5000.mnSNP1.mthbasepair-pca.MAF0.05.CSSm.10000perm.txt"
+#CSS.run <- "stickleback.wnd2500.sld500.mnSNP1.mthbasepair-mds.MAF0.05.CSSm.10000perm.txt"
 
 ## Read in chrom info
 chr <- as_tibble(read.table("/gpfs01/home/mbzcp2/data/sticklebacks/genomes/GCA_046562415.1_Duke_GAcu_1.0_genomic_sequence_report.tsv", sep = "\t", header = T))
@@ -28,39 +28,116 @@ CSS$bi.col <- chr$bi.col[match(CSS$chr, chr$GenBank.seq.accession)]
 
 CSS$chr <- factor(CSS$chr, levels = chr$Sequence.name)
 
-## Remove low quality snps
-CSS.HQ <- CSS[CSS$nsnps>=20&CSS$nsnps<=100,]
-## Calculate q values
-CSS.q <- qvalue(1-CSS.HQ$pval, fdr.level = 0.0001)
+p.hist <- ggplot(CSS) +
+  geom_histogram(aes(nsnps), bins = 100)
 
-## Filter out CSS
-CSS.HQ$qval.calc <- CSS.q$qvalues
-CSS.HQ$qval.sig <- CSS.q$significant
+ggsave("/gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/test.png", p.hist)
+
+## Remove low quality snps
+CSS.HQ <- CSS[CSS$nsnps>=10&CSS$nsnps<=200,]
+## Calculate q values
+CSS.q.05 <- qvalue(1-CSS.HQ$pval, fdr.level = 0.05)
+CSS.q.001 <- qvalue(1-CSS.HQ$pval, fdr.level = 0.001)
+CSS.q.0001 <- qvalue(1-CSS.HQ$pval, fdr.level = 0.0001)
+# Add to dataset
+CSS.HQ$qval.calc.05 <- CSS.q.05$qvalues
+CSS.HQ$qval.sig.05 <- CSS.q.05$significant
+CSS.HQ$qval.calc.001 <- CSS.q.001$qvalues
+CSS.HQ$qval.sig.001 <- CSS.q.001$significant
+CSS.HQ$qval.calc.0001 <- CSS.q.0001$qvalues
+CSS.HQ$qval.sig.0001 <- CSS.q.0001$significant
 
 ## Assign unique ID to each run of signifcant results (split by chromosome)
 CSS.HQ <- CSS.HQ %>%
-  mutate(temp = replace(qval.sig, is.na(qval.sig), FALSE), 
+  mutate(temp = replace(qval.sig.05, is.na(qval.sig.05), FALSE), 
          temp1 = cumsum(!temp)) %>%
   group_by(temp1) %>%
   mutate(goal =  +(row_number() == which.max(temp) & any(temp))) %>%
   group_by(chr) %>%
-  mutate(goal = ifelse(temp, cumsum(goal), NA)) %>%
+  mutate(goal.05 = ifelse(temp, paste0(first(chr), "_", cumsum(goal)), NA)) %>%
   select(-temp, -temp1)
 
+CSS.HQ <- CSS.HQ %>%
+  mutate(temp = replace(qval.sig.001, is.na(qval.sig.001), FALSE), 
+         temp1 = cumsum(!temp)) %>%
+  group_by(temp1) %>%
+  mutate(goal =  +(row_number() == which.max(temp) & any(temp))) %>%
+  group_by(chr) %>%
+  mutate(goal.001 = ifelse(temp, paste0(first(chr), "_", cumsum(goal)), NA)) %>%
+  select(-temp, -temp1)
+
+CSS.HQ <- CSS.HQ %>%
+  mutate(temp = replace(qval.sig.0001, is.na(qval.sig.0001), FALSE), 
+         temp1 = cumsum(!temp)) %>%
+  group_by(temp1) %>%
+  mutate(goal =  +(row_number() == which.max(temp) & any(temp))) %>%
+  group_by(chr) %>%
+  mutate(goal.0001 = ifelse(temp, paste0(first(chr), "_", cumsum(goal)), NA)) %>%
+  select(-temp, -temp1)
+
+## How many windows are there?
+nrow(CSS.HQ)
+## Range of SNPs
+summary(CSS.HQ$nsnps)
+mean(CSS.HQ$nsnps)
+sd(CSS.HQ$nsnps)
+# Mean CSS score
+summary(CSS.HQ$css)
+# Percentage were significant
+(sum(CSS.HQ$qval.sig.05)/nrow(CSS.HQ))*100
+(sum(CSS.HQ$qval.sig.001)/nrow(CSS.HQ))*100
+(sum(CSS.HQ$qval.sig.0001)/nrow(CSS.HQ))*100
+
+## Split into X number of regions
+length(unique(CSS.HQ$goal.0001))
+length(unique(CSS.HQ$goal.0001[CSS.HQ$css>=2]))
+# Length distribution of signigicant windows
+sort(table(CSS.HQ$goal.0001), decreasing = T)*2500
+(sort(table(CSS.HQ$goal.0001), decreasing = T)*2500)[1:10]
+summary(as.numeric(sort(table(CSS.HQ$goal.0001), decreasing = T)*2500))
+
+## Top regions
+CSS.HQ$goal.0001[CSS.HQ$css>=2]
+top.regions <- sort(table(CSS.HQ$goal.0001[CSS.HQ$css>=2]), decreasing = T)*2500
+top.regions <- top.regions[top.regions>=(2500*(2500/500))]
+length(top.regions)
+CSS.HQ$top.regions <- CSS.HQ$goal.0001 %in% names(top.regions)
+unique(CSS.HQ$chr[CSS.HQ$top.regions])
+
+## Table
+top.regions.table <- CSS.HQ %>%
+  filter(top.regions) %>%
+  group_by(goal.0001) %>%
+  summarise(chr = first(chr),
+            start = min(start),
+            end = max(end),
+            mn.CSS = mean(css),
+            wnd.length = 1+max(end)-min(start)) %>%
+  ungroup() %>%
+  arrange(chr, start)
+  
+# Write out file
+top.regions.table %>% 
+  select(-goal.0001) %>%
+  mutate(mn.CSS = round(mn.CSS, 3)) %>%
+  write.table(file = paste0(CSS.dir,"/",gsub(".txt","",CSS.run),"_top_regions.txt"), row.names = F, quote = F)
+
 p <- ggplot(CSS.HQ) +
-  geom_point(aes(start, css, col = qval.sig)) +
+  geom_point(aes(start, css, col = qval.sig.0001)) +
+  geom_point(data = CSS.HQ[CSS.HQ$top.regions,], aes(start, css), col = "firebrick") +
   facet_grid(.~chr, scale = "free_x", space = "free_x") +
   scale_color_manual(values=c("grey50", "deepskyblue")) +
   scale_x_continuous(labels = function(x) paste0(x / 1e6), breaks = c(seq(0, max(chr$Seq.length),10e6)),name = "Mbs") +
   theme_classic() +
-  theme(legend.position = "top",panel.spacing = unit(0,'lines')) +
+  theme(legend.position = "none",panel.spacing = unit(0,'lines')) +
   ggtitle(gsub(".txt","",CSS.run))
 
 ggsave(paste0(CSS.dir,"/",gsub(".txt","",CSS.run),".pdf"), p, height = 5, width = 15)
 ggsave(paste0(CSS.dir,"/",gsub(".txt","",CSS.run),".png"), p, height = 5, width = 15)
 
 p <- ggplot(CSS.HQ) +
-  geom_point(aes(start, css, col = qval.sig)) +
+  geom_point(aes(start, css, col = qval.sig.0001)) +
+    geom_point(data = CSS.HQ[CSS.HQ$top.regions,], aes(start, css), col = "firebrick") +
   facet_grid(chr~., scale = "free_x", space = "free_x") +
   scale_color_manual(values=c("grey50", "deepskyblue")) +
   scale_x_continuous(labels = function(x) paste0(x / 1e6), breaks = c(seq(0, max(chr$Seq.length),10e6)),name = "Mbs") +
