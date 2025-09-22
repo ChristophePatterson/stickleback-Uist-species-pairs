46a6ac58fc76b1d310e9cc801a4b557fe385b8cc
Christophe Patterson <christophepatterson@outlook.com>
2025-09-22 13:45:46 +0100

    RaxML threads update

--- 2_Results/2_2_Divergence_estimates/16.0-RAxML.sh
+++ 2_Results/2_2_Divergence_estimates/16.0-RAxML.sh
@@ -6,9 +6,9 @@
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=58
-#SBATCH --mem=10g
-#SBATCH --time=72:00:00
+#SBATCH --cpus-per-task=93
+#SBATCH --mem=25g
+#SBATCH --time=167:00:00
 #SBATCH --job-name=RAxML
 #SBATCH --output=/gpfs01/home/mbzcp2/slurm_outputs/slurm-%x-%j.out
 
@@ -37,7 +37,7 @@ pwd
 echo $phy_file
 
 # raxml-ng --check --msa $wkdir/vcfs/$vcf_ver/$phy_file --model GTGTR4+G+ASC_LEWIS --threads $SLURM_CPUS_PER_TASK --prefix GTGTR4_G_ASC_LEWIS
-# raxml-ng --parse --msa $wkdir/vcfs/$vcf_ver/$phy_file --model GTGTR4+G+ASC_LEWIS --threads $SLURM_CPUS_PER_TASK --prefix GTGTR4_G_ASC_LEWIS
+# raxml-ng --parse --msa $wkdir/vcfs/$vcf_ver/$phy_file --model GTGTR4+G+ASC_LEWIS --prefix GTGTR4_G_ASC_LEWIS
 raxml-ng --all --msa $wkdir/vcfs/$vcf_ver/$phy_file --model GTGTR4+G+ASC_LEWIS --bs-trees 500 --tree pars{10},rand{10} --threads $SLURM_CPUS_PER_TASK --prefix ${SNP_library}_raxml_GTGTR4_G_ASC_LEWIS_BS500_P10R10
 
 ## Map bootstrap values on the ML tree
