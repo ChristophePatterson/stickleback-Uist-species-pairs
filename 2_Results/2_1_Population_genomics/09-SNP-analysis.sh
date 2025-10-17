a4234037b5b2cb7a40c8ccd50fe6492e2aa0b3a4
Christophe Patterson <christophepatterson@outlook.com>
2025-10-17 13:55:01 +0100

    Filtering out High Fst regions  from LEA and PCA results

--- 2_Results/2_1_Population_genomics/09-SNP-analysis.sh
+++ 2_Results/2_1_Population_genomics/09-SNP-analysis.sh
@@ -17,7 +17,7 @@
 ############################
 
 # load modules
-
+source /gpfs01/home/${USER}/.bashrc
 conda activate bcftools-env
 
 # set variables
@@ -70,65 +70,44 @@ module load R-uoneasy/4.2.1-foss-2022a
 ## Custom analysis
 
 ## Masked data
+#### Rscript /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/2_Results/2_1_Population_genomics/09-SNP-analysis.R \
+####        $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.masked.rand1000.geno $vcf_ver
+#### 
+#### # Unmasked data
+#### Rscript /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/2_Results/2_1_Population_genomics/09-SNP-analysis.R \
+####         $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.rand1000.geno $vcf_ver
+
+
+## Conduct analysis on hard filtered FST outliers
+module purge
+conda activate bcftools-env
+
+# Set FST outlier threshold
+FstUpper="0.1"
+
+## Get regions of genome that are FST outliers in bed format
+awk -F "," -v OFS='\t' -v Fst=$FstUpper 'NR!=1 && $9!="nan" && $9 >= (Fst + 0) { print $1, $2, $3, $9}' \
+    $wkdir/results/$vcf_ver/sliding-window/sliding_window_w25kb_s5kb_m1_Panad_resi_auto.csv > \
+    $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.FST.$FstUpper.outliers.bed
+
+## Filter out snps from these regions
+bcftools view -T ^$wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.FST.$FstUpper.outliers.bed \
+    -O z -o $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.noFST$FstUpper.vcf.gz \
+    $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.vcf.gz
+
+## Filter to get 1 snp per 1kb
+bcftools +prune -n 1 -N rand -w 1000bp -O v -o $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.noFST$FstUpper.rand1000.vcf.gz \
+    $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.noFST$FstUpper.vcf.gz
+
+# Load R module
+module purge
+module load R-uoneasy/4.2.1-foss-2022a
+
+# Convert to geno format
+Rscript /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/1_Mapping_and_calling/08.1-vcf2geno.R \
+    $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.noFST$FstUpper.rand1000.vcf.gz \
+    $vcf_ver
+
+# Run LEA analysis
 Rscript /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/2_Results/2_1_Population_genomics/09-SNP-analysis.R \
-       $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.masked.rand1000.geno $vcf_ver
-
-# Unmasked data
-Rscript /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/2_Results/2_1_Population_genomics/09-SNP-analysis.R \
-        $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.rand1000.geno $vcf_ver
-
-#####################
-  # fastStructure  # 
-#####################
-
-## conda create -n faststucture-p2 python=2
-###### module purge
-###### conda activate faststucture-p2
-###### module load bcftools-uoneasy/1.18-GCC-13.2.0
-###### module load plink-uoneasy/2.00a3.7-foss-2023a-highcontig
-###### 
-###### outdir=/gpfs01/home/mbzcp2/data/sticklebacks/results/faststructure
-###### mkdir -p $outdir
-###### 
-###### ## Create input file for fasstructure
-###### inputfile=(stickleback_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.rand1000)
-###### 
-###### ## Remove non paired species 
-###### bcftools view -S $wkdir/vcfs/${species}_subset_samples.txt $wkdir/vcfs/${inputfile}.vcf.gz | \
-######     bcftools view --min-ac 2[minor] -O z -o $wkdir/vcfs/${inputfile}.rand1000.SpPair.vcf.gz
-###### 
-###### ## Convert to plink format
-###### plink -vcf  $wkdir/vcfs/${inputfile}.rand1000.SpPair.vcf.gz --allow-extra-chr -recode --out $wkdir/vcfs/plink/${inputfile}.rand1000.SpPair
-###### plink --file $wkdir/vcfs/plink/${inputfile}.rand1000.SpPair --chr-set 95 --allow-extra-chr --make-bed --recode A --out $wkdir/vcfs/plink/${inputfile}.rand1000.SpPair.recode
-###### 
-###### ## Create popfile for plotting results
-###### grep -f $wkdir/vcfs/${species}_subset_samples.txt /gpfs01/home/mbzcp2/code/Github/stickleback-Uist-species-pairs/bigdata_Christophe_2025-04-28.csv | 
-######     awk -F ',' -v OFS='\t' '{ print $10 }' > $outdir/popfile.txt
-###### 
-###### ## Loop through running faststructure with K= 1 to 5
-###### # Set i as 0
-###### i=0
-###### ## Start looop
-###### while [ $i -ne 20 ]
-###### do
-######         ## Add 1 to K
-######         i=$(($i+1))
-######         echo "Running faststructre for K-$i"
-######         ## Run structure
-######         structure.py --input=$wkdir/vcfs/plink/${inputfile}.rand1000.SpPair.recode \
-######             --format bed -K $i --output=$outdir/${species}_SpPair \
-######             --cv=20 --full
-###### 
-######         ## Plot stucture make sure you have added code to distruct.py
-######         # beforethe to the code "import matplotlib.pyplot as plot"
-######         # #import matplotlib as mpl
-######         # #mpl.use('Agg')
-###### 
-######         distruct.py --input=$outdir/${species}_SpPair \
-######              -K $i --output=$outdir/${species}_SpPair_K${i}.pdf --popfile=$outdir/popfile.txt \
-######              --title=${species}_SpPair_K${i}
-###### 
-###### done
-###### 
-###### conda deactivate
-###### 
+        $wkdir/vcfs/$vcf_ver/${species}_SNPs.NOGTDP5.MEANGTDP5_200.Q60.SAMP0.8.MAF2.noFST$FstUpper.rand1000.geno $vcf_ver
