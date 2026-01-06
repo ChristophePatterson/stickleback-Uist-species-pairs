6eeef3ddf3150c8e267df66e285eed50be1cf03a
Christophe Patterson <christophepatterson@outlook.com>
2026-01-06 10:16:56 +0000

    Param est and tpl now work, remember to set popID from 0 and adding weird symbols to param names just because that how fsc rolls

--- 2_Results/2_1_Population_genomics/18.2-fastsimcoal-allresi-single-mig.sh
+++ 2_Results/2_1_Population_genomics/18.2-fastsimcoal-allresi-single-mig.sh
@@ -6,9 +6,9 @@
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=60g
-#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=100g
+#SBATCH --time=72:00:00
 #SBATCH --job-name=fastsimcoal2-allpops
 #SBATCH --output=/gpfs01/home/mbzcp2/slurm_outputs/slurm-%x-%j.out
 
@@ -24,12 +24,12 @@ wkdir=/gpfs01/home/mbzcp2/data/sticklebacks
 species=stickleback
 genome_name=(GCA_046562415.1_Duke_GAcu_1.0_genomic)
 vcf_ver=($genome_name/ploidy_aware_HWEPops_MQ10_BQ20)
-randSNP=1000000
+randSNP=100000
 
 # folded or unfold
 foldtype=("folded")
 # Analyse name
-analysis_name=allpops
+analysis_name=allresi_singlemig_N${SLURM_CPUS_PER_TASK}
 
 ## Output
 output_dir=($wkdir/results/$vcf_ver/demographic/fastsimcoal2/${analysis_name}_r${randSNP})
@@ -144,37 +144,38 @@ projSFSsites=$(awk 'NR>=3 {print $0}' ${analysis_name}_${foldtype}_MSFS.obs   | 
 
 cat $output_dir/pop_uniq.txt
 ## Create model parameters file
-echo "//Parameters for the coalescence simulation program : simcoal.exe" > $output_dir/fsc_run/${analysis_name}.tpl
-echo "5 samples to simulate :" >> $output_dir/fsc_run/${analysis_name}.tpl
-echo "//Population effective sizes (number of genes)" >> $output_dir/fsc_run/${analysis_name}.tpl
+echo "//Parameters for the coalescence simulation program : simcoal.exe" > $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "5 samples to simulate :" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "//Population effective sizes (number of genes)" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
 ## Print out NPOP lines from waterbody uniq file
-awk '{print "NPOP"$1}' $output_dir/pop_uniq.txt >> $output_dir/fsc_run/${analysis_name}.tpl
-echo "//Samples sizes and samples age" >> $output_dir/fsc_run/${analysis_name}.tpl
-awk '{print $2}' $output_dir/best_proj_$foldtype.txt >> $output_dir/fsc_run/${analysis_name}.tpl
-echo "//Growth rates: negative growth implies population expansion" >> $output_dir/fsc_run/${analysis_name}.tpl
-awk '{print "0"}' $output_dir/pop_uniq.txt >> $output_dir/fsc_run/${analysis_name}.tpl
-echo "//Number of migration matrices : 0 implies no migration between demes" >> $output_dir/fsc_run/${analysis_name}.tpl
-echo "0" >> $output_dir/fsc_run/${analysis_name}.tpl
+awk '{print $1"$"}' $output_dir/pop_uniq.txt >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "//Samples sizes and samples age" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+awk '{print $2}' $output_dir/best_proj_$foldtype.txt >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "//Growth rates: negative growth implies population expansion" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+awk '{print "0"}' $output_dir/pop_uniq.txt >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "//Number of migration matrices : 0 implies no migration between demes" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "0" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
 # Historical events
-echo "//historical event: time, source, sink, migrants, new deme size, growth rate, migr mat index" >> $output_dir/fsc_run/${analysis_name}.tpl
-echo "4 historical event" >> $output_dir/fsc_run/${analysis_name}.tpl
-# CLAC merges into LUIB
-echo "TDivRWest $(awk '$1=="CLAC" {print NR}' $output_dir/pop_uniq.txt) $(awk '$1=="LUIB" {print NR}' $output_dir/pop_uniq.txt) 1 RESIZE1 0 1" >> $output_dir/fsc_run/${analysis_name}.tpl
+echo "//historical event: time, source, sink, migrants, new deme size, growth rate, migr mat index" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "4 historical event" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+# Get population numbers for events (remember fsc starts counting at 0)
 #OBSE merges into DUIN
-echo "TDivREast $(awk '$1=="OBSE" {print NR}' $output_dir/pop_uniq.txt) $(awk '$1=="DUIN" {print NR}' $output_dir/pop_uniq.txt) 1 RESIZE2 0 1" >> $output_dir/fsc_run/${analysis_name}.tpl
+echo "TDivResiEast£ $(awk '$1=="OBSE" {print NR-1}' $output_dir/pop_uniq.txt) $(awk '$1=="DUIN" {print NR-1}' $output_dir/pop_uniq.txt) 1 RESIZE2 0 0" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+# CLAC merges into LUIB
+echo "TDivResiWest£ $(awk '$1=="CLAC" {print NR-1}' $output_dir/pop_uniq.txt) $(awk '$1=="LUIB" {print NR-1}' $output_dir/pop_uniq.txt) 1 RESIZE1 0 0" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
 # Resi west and east merge
-echo "TDivResi $(awk '$1=="LUIB" {print NR}' $output_dir/pop_uniq.txt) $(awk '$1=="DUIN" {print NR}' $output_dir/pop_uniq.txt) 1 RESIZE3 0 1" >> $output_dir/fsc_run/${analysis_name}.tpl
+echo "TDivResi£ $(awk '$1=="LUIB" {print NR-1}' $output_dir/pop_uniq.txt) $(awk '$1=="DUIN" {print NR-1}' $output_dir/pop_uniq.txt) 1 RESIZE3 0 0" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
 # Resi and Migr merge into Ancestral
-echo "TDivAncs $(awk '$1=="DUIN" {print NR}' $output_dir/pop_uniq.txt) $(awk '$1=="anad" {print NR}' $output_dir/pop_uniq.txt) 1 RESIZE4 0 1" >> $output_dir/fsc_run/${analysis_name}.tpl
+echo "TDivAncs£ $(awk '$1=="DUIN" {print NR-1}' $output_dir/pop_uniq.txt) $(awk '$1=="anad" {print NR-1}' $output_dir/pop_uniq.txt) 1 RESIZE4 0 0" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
 
-echo "//Number of independent loci [chromosome]" >> $output_dir/fsc_run/${analysis_name}.tpl
-echo "1 0" >> $output_dir/fsc_run/${analysis_name}.tpl
-echo "//Per chromosome: Number of contiguous linkage Block: a block is a set of contiguous loci" >> $output_dir/fsc_run/${analysis_name}.tpl
-echo "1" >> $output_dir/fsc_run/${analysis_name}.tpl
-echo "//per Block:data typ" >> $output_dir/fsc_run/${analysis_name}.tpl
+echo "//Number of independent loci [chromosome]" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "1 0" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "//Per chromosome: Number of contiguous linkage Block: a block is a set of contiguous loci" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "1" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
+echo "//per Block:data typ" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
 
 ###### TO FIX ######
-echo "FREQ $projSFSsites 0 5.11e-9 OUTEXP"  >> $output_dir/fsc_run/${analysis_name}.tpl
+echo "FREQ $projSFSsites 0 5.11e-9 OUTEXP"  >> $output_dir/fsc_run/${analysis_name}_${foldtype}.tpl
 
 ############################
  ## Create estimates file ##
@@ -184,32 +185,33 @@ echo "FREQ $projSFSsites 0 5.11e-9 OUTEXP"  >> $output_dir/fsc_run/${analysis_na
 minNPOP=1000
 maxNPOP=1000000
 
-echo "// Priors and rules file" > $output_dir/fsc_run/${analysis_name}.est
-echo "// *********************" >> $output_dir/fsc_run/${analysis_name}.est
-echo "[PARAMETERS]" >> $output_dir/fsc_run/${analysis_name}.est
-echo "//#isInt? #name #dist.#min #max" >> $output_dir/fsc_run/${analysis_name}.est
-echo "//all N are in number of haploid individuals" >> $output_dir/fsc_run/${analysis_name}.est
-## All popuulations
-awk -v mxNPOP=$maxNPOP '{print "1 NPOP"$1" unif 1000 "mxNPOP" output"}' $output_dir/pop_uniq.txt >> $output_dir/fsc_run/${analysis_name}.est
-# East and west ancestral sizes
-echo "1 ResiWest unif 1000 $maxNPOP output" >> $output_dir/fsc_run/${analysis_name}.est
-echo "1 ResiEast unif 1000 $maxNPOP output" >> $output_dir/fsc_run/${analysis_name}.est
+echo "// Priors and rules file" > $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "// *********************" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "[PARAMETERS]" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "//#isInt? #name #dist.#min #max" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "//all N are in number of haploid individuals" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
 # Ecotype ancestral sizes
-echo "1 Resi unif 1000 $maxNPOP output" >> $output_dir/fsc_run/${analysis_name}.est
-echo "1 Ancs unif 1000 $maxNPOP output" >> $output_dir/fsc_run/${analysis_name}.est
+echo "1 Ancs$ unif 1000 $maxNPOP output" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "1 Resi$ unif 1000 $maxNPOP output" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+# East and west ancestral sizes
+echo "1 ResiWest$ unif 1000 $maxNPOP output" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "1 ResiEast$ unif 1000 $maxNPOP output" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+
+## All popuulations
+awk -v mxNPOP=$maxNPOP '{print "1 "$1"$ unif 1000 "mxNPOP" output"}' $output_dir/pop_uniq.txt >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
 
 # Divergence times
-echo "1 TDivRWest unif 1000 TDivResi output paramInRange">> $output_dir/fsc_run/${analysis_name}.est
-echo "1 TDivREast unif 1000 TDivResi output paramInRange">> $output_dir/fsc_run/${analysis_name}.est
-echo "1 TDivResi unif 1000 TDivAncs output paramInRange" >> $output_dir/fsc_run/${analysis_name}.est
-echo "1 TDivAncs unif 1000 200000 output" >> $output_dir/fsc_run/${analysis_name}.est
+echo "1 TDivAncs£ unif 1000 200000 output" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "1 TDivResi£ unif 1000 TDivAncs£ output paramInRange" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "1 TDivResiWest£ unif 1000 TDivResi£ output paramInRange">> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "1 TDivResiEast£ unif 1000 TDivResi£ output paramInRange">> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
 
 #  Complex parameters
-echo "[COMPLEX PARAMETERS]" >> $output_dir/fsc_run/${analysis_name}.est
-echo "0 RESIZE1 = ResiWest/NPOPLUIB hide" >> $output_dir/fsc_run/${analysis_name}.est
-echo "0 RESIZE2 = ResiWest/NPOPDUIN hide" >> $output_dir/fsc_run/${analysis_name}.est
-echo "0 RESIZE3 = Resi/ResiWest hide" >> $output_dir/fsc_run/${analysis_name}.est
-echo "0 RESIZE4 = Ancs/anad hide" >> $output_dir/fsc_run/${analysis_name}.est
+echo "[COMPLEX PARAMETERS]" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "0 RESIZE1 = ResiWest$/LUIB$ hide" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "0 RESIZE2 = ResiWest$/DUIN$ hide" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "0 RESIZE3 = Resi$/ResiWest$ hide" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
+echo "0 RESIZE4 = Ancs$/anad$ hide" >> $output_dir/fsc_run/${analysis_name}_${foldtype}.est
 
 ## Run fsc
-~/apps/fsc28_linux64/fsc28 -t ${analysis_name}.tpl -n 1000000 -e ${analysis_name}.est -m -u -M -L 1000 -c $SLURM_CPUS_PER_TASK -q 
+~/apps/fsc28_linux64/fsc28 -t ${analysis_name}_${foldtype}.tpl -n 1000000 -e ${analysis_name}_${foldtype}.est -m -u -M -L 1000 -c $SLURM_CPUS_PER_TASK -q 
