3e428fa840906cf148155f88470ab0e41cd2848c
Christophe Patterson <christophepatterson@outlook.com>
2026-01-19 12:33:10 +0000

    Monphyletic loch fsc code

---
+++ 2_Results/2_1_Population_genomics/18.6-fastsimcoal-all-monophy-loch.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+# Christophe Patterson
+# 10/04/25
+# for running on the UoN HPC Ada
+
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=60g
+#SBATCH --time=24:00:00
+#SBATCH --array=1-100
+#SBATCH --job-name=fastsimcoal2-all
+#SBATCH --output=/gpfs01/home/mbzcp2/slurm_outputs/slurm-%x-%j.out
+
+############################
+   # PREPARE ENVIRONMENT #
+############################
+
+module purge
+source /gpfs01/home/${USER}/.bashrc
+
+# set variables
+wkdir=/gpfs01/home/mbzcp2/data/sticklebacks
+species=stickleback
+genome_name=(GCA_046562415.1_Duke_GAcu_1.0_genomic)
+vcf_ver=($genome_name/ploidy_aware_HWEPops_MQ10_BQ20)
+randSNP=10000
+
+# folded or unfold
+foldtype=("unfolded")
+# Global output directory
+output_dir=($wkdir/results/$vcf_ver/demographic/fastsimcoal2/model_selection_${foldtype}_r${randSNP})
+
+output_model=($output_dir/model_files/run_${foldtype}_A${SLURM_ARRAY_TASK_ID})
+analysis_name=SFS_${SLURM_ARRAY_TASK_ID}_all-monophy-loch_${foldtype}
+
+## Check is SFS directory exists
+if [ ! -d $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/SFS_all_$foldtype/ ]; then
+    echo "SFS directory does not exist: $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/SFS_all_$foldtype/"
+    echo "Please run 18.0-fastsimcoal-setup.sh first to generate the SFS."
+    exit 1
+fi
+
+############################
+  ## Create model files ##
+############################
+
+mkdir -p $output_model
+cd $output_model
+
+## Remove old obs
+rm -f ./*.obs
+
+# Copy over jointMAF file to fsc run directory
+for file in $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/SFS_all_$foldtype/fastsimcoal2/*joint*.obs; do
+    # copy and rename files from "all" to "all-monophy-loch"
+    # And move into fsc run directory
+    # Get file basename
+    basefile=$(basename "$file")
+    echo "$basefile"
+    cp "$file" ./"${basefile/all/all-monophy-loch}"
+done
+
+## Create model parameters file
+echo "//Parameters for the coalescence simulation program : simcoal.exe" > $output_model/${analysis_name}.tpl
+echo "8 samples to simulate :" >> $output_model/${analysis_name}.tpl
+echo "//Population effective sizes (number of genes)" >> $output_model/${analysis_name}.tpl
+## Print out NPOP lines from waterbody uniq file
+awk '{print $1"$"}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt >> $output_model/${analysis_name}.tpl
+echo "//Samples sizes and samples age" >> $output_model/${analysis_name}.tpl
+awk '{print $2}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/SFS_${SLURM_ARRAY_TASK_ID}_all_best_proj_unfolded.txt >> $output_model/${analysis_name}.tpl
+echo "//Growth rates: negative growth implies population expansion" >> $output_model/${analysis_name}.tpl
+awk '{print "0"}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt >> $output_model/${analysis_name}.tpl
+echo "//Number of migration matrices : 0 implies no migration between demes" >> $output_model/${analysis_name}.tpl
+echo "0" >> $output_model/${analysis_name}.tpl
+# Historical events
+echo "//historical event: time, source, sink, migrants, new deme size, growth rate, migr mat index" >> $output_model/${analysis_name}.tpl
+echo "7 historical event" >> $output_model/${analysis_name}.tpl
+# Get population numbers for events (remember fsc starts counting at 0)
+# CLAC merges into LUIB
+echo "TDivCLAC@ $(awk '$1=="CLAC" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) $(awk '$1=="CLAM" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) 1 RESIZE1 0 0" >> $output_model/${analysis_name}.tpl
+#OBSE merges into DUIN
+echo "TDivLUIB@ $(awk '$1=="LUIB" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) $(awk '$1=="LUIM" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) 1 RESIZE2 0 0" >> $output_model/${analysis_name}.tpl
+# CLAM merges into LUIM
+echo "TDivDUIN@ $(awk '$1=="DUIN" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) $(awk '$1=="DUIM" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) 1 RESIZE3 0 0" >> $output_model/${analysis_name}.tpl
+# OBSM merges into DUIM
+echo "TDivOBSE@ $(awk '$1=="OBSE" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) $(awk '$1=="OBSM" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) 1 RESIZE4 0 0" >> $output_model/${analysis_name}.tpl
+# Resi west and east merge
+echo "TDivWest@ $(awk '$1=="CLAM" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) $(awk '$1=="LUIM" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) 1 RESIZE5 0 0" >> $output_model/${analysis_name}.tpl
+# Migration west and east merge
+echo "TDivEast@ $(awk '$1=="DUIM" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) $(awk '$1=="OBSM" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) 1 RESIZE6 0 0" >> $output_model/${analysis_name}.tpl
+# Resi and Migr merge into Ancestral
+echo "TDivAncs@ $(awk '$1=="LUIM" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) $(awk '$1=="OBSM" {print NR-1}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt) 1 RESIZE7 0 0" >> $output_model/${analysis_name}.tpl
+
+echo "//Number of independent loci [chromosome]" >> $output_model/${analysis_name}.tpl
+echo "1 0" >> $output_model/${analysis_name}.tpl
+echo "//Per chromosome: Number of contiguous linkage Block: a block is a set of contiguous loci" >> $output_model/${analysis_name}.tpl
+echo "1" >> $output_model/${analysis_name}.tpl
+echo "//per Block:data typ" >> $output_model/${analysis_name}.tpl
+
+echo "FREQ 1 0 5.11e-9 OUTEXP"  >> $output_model/${analysis_name}.tpl
+
+############################
+ ## Create estimates file ##
+############################
+
+# Set min and max population sizes
+minNPOP=1000
+maxNPOP=1000000
+
+echo "// Priors and rules file" > $output_model/${analysis_name}.est
+echo "// *********************" >> $output_model/${analysis_name}.est
+echo "[PARAMETERS]" >> $output_model/${analysis_name}.est
+echo "//#isInt? #name #dist.#min #max" >> $output_model/${analysis_name}.est
+echo "//all N are in number of haploid individuals" >> $output_model/${analysis_name}.est
+## All popuulations
+awk -v mxNPOP=$maxNPOP -v mnNPOP=$minNPOP '{print "1 "$1"$ unif "mnNPOP" "mxNPOP" output"}' $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt >> $output_model/${analysis_name}.est
+# East and west ancestral sizes
+echo "1 CLACAncs$ unif $minNPOP $maxNPOP output" >> $output_model/${analysis_name}.est
+echo "1 LUIBAncs$ unif $minNPOP $maxNPOP output" >> $output_model/${analysis_name}.est
+echo "1 DUINAncs$ unif $minNPOP $maxNPOP output" >> $output_model/${analysis_name}.est
+echo "1 OBSEAncs$ unif $minNPOP $maxNPOP output" >> $output_model/${analysis_name}.est
+# Ecotype ancestral sizes
+echo "1 West$ unif $minNPOP $maxNPOP output" >> $output_model/${analysis_name}.est
+echo "1 East$ unif $minNPOP $maxNPOP output" >> $output_model/${analysis_name}.est
+echo "1 AncsAll$ unif $minNPOP $maxNPOP output" >> $output_model/${analysis_name}.est
+
+# Divergence times (ordered from oldest to most recent)
+# The lower range limit is an absolute minimum, whereas the upper range is only used as a
+# maximum for choosing a random initial value for this parameter. There is actually no upper
+# limit to the search range, as this limit can grow by 30% after each cycle
+echo "1 TDivAncs@ unif 100 2000000 output">> $output_model/${analysis_name}.est
+echo "1 TDivWest@ unif 100 TDivAncs@ output paramInRange">> $output_model/${analysis_name}.est
+echo "1 TDivEast@ unif 100 TDivAncs@ output paramInRange">> $output_model/${analysis_name}.est
+echo "1 TDivCLAC@ unif 100 TDivWest@ output" paramInRange >> $output_model/${analysis_name}.est
+echo "1 TDivLUIB@ unif 100 TDivWest@ output paramInRange" >> $output_model/${analysis_name}.est
+echo "1 TDivDUIN@ unif 100 TDivEast@ output paramInRange" >> $output_model/${analysis_name}.est
+echo "1 TDivOBSE@ unif 100 TDivEast@ output paramInRange">> $output_model/${analysis_name}.est
+
+#  Complex parameters
+echo "[COMPLEX PARAMETERS]" >> $output_model/${analysis_name}.est
+echo "0 RESIZE1 = CLAC$/CLAM$ hide" >> $output_model/${analysis_name}.est
+echo "0 RESIZE2 = LUIB$/LUIM$ hide" >> $output_model/${analysis_name}.est
+echo "0 RESIZE3 = DUIN$/DUIM$ hide" >> $output_model/${analysis_name}.est
+echo "0 RESIZE4 = OBSE$/OBSM$ hide" >> $output_model/${analysis_name}.est
+echo "0 RESIZE5 = CLAM$/LUIM$ hide" >> $output_model/${analysis_name}.est
+echo "0 RESIZE6 = DUIM$/OBSM$ hide" >> $output_model/${analysis_name}.est
+echo "0 RESIZE7 = LUIM$/OBSM$ hide" >> $output_model/${analysis_name}.est
+
+##### 
+## Run fsc
+if [[ $foldtype == "folded" ]]; then
+~/apps/fsc28_linux64/fsc28 -t ${analysis_name}.tpl -n 100000 -e ${analysis_name}.est -y 4 --foldedSFS -m -M -L 50 -c $SLURM_CPUS_PER_TASK > $output_model/fsc_${analysis_name}_log_jobID${SLURM_ARRAY_TASK_ID}.txt
+fi
+if [[ $foldtype == "unfolded" ]]; then
+~/apps/fsc28_linux64/fsc28 -t ${analysis_name}.tpl -n 100000 -e ${analysis_name}.est -y 4 -d -M -L 50 -c $SLURM_CPUS_PER_TASK > $output_model/fsc_${analysis_name}_log_jobID${SLURM_ARRAY_TASK_ID}.txt
+fi
+
+############################
+ ##### Plot results #####
+############################
+
+# Move into fsc run directory
+cd $output_model/${analysis_name}
+
+# Load R module
+module load R-uoneasy/4.2.1-foss-2022a
+## Plot maxPar file
+Rscript ~/code/Github/stickleback-Uist-species-pairs/Helper_scripts/ParFileViewer.R ${analysis_name}_maxL.par $output_dir/SFS/SFS_${SLURM_ARRAY_TASK_ID}/pop_all_uniq.txt
+
+# Copy results to aggregate results directory
+mkdir -p $output_dir/results_plots/
+
+cp ${analysis_name}.bestlhoods $output_dir/results_plots/
+cp ${analysis_name}_maxL.par $output_dir/results_plots/
+cp ${analysis_name}_maxL.par.pdf $output_dir/results_plots/
+
+##### 
+##### ### Then once all jobs are done, run:
+##### Rscript ~/code/Github/stickleback-Uist-species-pairs/2_Results/2_1_Population_genomics/18.15-fastsimcoal-all-bootstrap-plot.R \
+#####     $output_dir/results_plots/ \
+#####     ${analysis_name}
